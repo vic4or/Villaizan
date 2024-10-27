945728104cf016525681406da469b7ba5fd48a80
vic4or <victor.avalos@pucp.edu.pe>
2024-10-27 14:48:40 -0500

    agregar frutas / correcciones producto - punto

--- backend/src/producto/producto.service.ts
+++ backend/src/producto/producto.service.ts
@@ -8,7 +8,7 @@ export class ProductoService {
     constructor(private prisma: PrismaService) {}
 
     async getAllProductos(): Promise<vi_producto[]> {
-        return this.prisma.vi_producto.findMany({
+        return await this.prisma.vi_producto.findMany({
             where: {
                 estado: true
             }
