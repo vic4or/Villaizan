e9be1466db0868e75fa5b6aa05d07f0042ccac85
vic4or <victor.avalos@pucp.edu.pe>
2024-10-31 02:31:29 -0500

    cambios registrar y editar frutas

--- backend/src/fruta/fruta.controller.ts
+++ backend/src/fruta/fruta.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Body, Param } from '@nestjs/common';import { FrutaService } from './fruta.service';
+import { Controller, Get, Post, Put, Body, Param, Patch } from '@nestjs/common';import { FrutaService } from './fruta.service';
 import { vi_fruta } from '@prisma/client';
 
 @Controller('fruta')
@@ -19,17 +19,24 @@ export class FrutaController {
     @Post('/registrar')
     async registrarFruta(
         @Body('nombre') nombre: string,
-        @Body('descripcion') descripcion: string
+        @Body('descripcion') descripcion?: string,
+        @Body('productos') productos?: string[],
     ) {
-        return await this.frutaService.createFruta(nombre, descripcion);
+        return await this.frutaService.createFruta(nombre, descripcion,productos);
     }
 
+    
+    @Patch('/editar/:id')
+    async editarFruta(
+        @Param('id') id: string,
+        @Body('nombre') nombre?: string,
+        @Body('descripcion') descripcion?: string,
+        @Body('productosParaAgregar') productosParaAgregar?: string[],
+        @Body('productosParaQuitar') productosParaQuitar?: string[],
+    ) {
+        return await this.frutaService.updateFruta(id, nombre, descripcion, productosParaAgregar, productosParaQuitar);
+    }
     /*
-    @Put('/editar/:id')
-    async editarFruta(@Param('id') id: string, @Body() data: Partial<vi_fruta>) {
-        return this.frutaService.updateFruta(id, data);
-    }
-
     @Put('/inactivar/:id')
     async inactivarFruta(@Param('id') id: string) {
         return this.frutaService.inactivateFruta(id);
--- backend/src/fruta/fruta.service.ts
+++ backend/src/fruta/fruta.service.ts
@@ -9,48 +9,139 @@ export class FrutaService {
     constructor(private prisma: PrismaService) {}
 
     async getAllFrutas(): Promise<vi_fruta[]> {
-        return await this.prisma.vi_fruta.findMany({
-        where: { estaactivo: true },  // Solo frutas activas
-        });
+        return await this.prisma.vi_fruta.findMany();
     }
 
     async getFrutaById(id: string): Promise<vi_fruta> {
         return await this.prisma.vi_fruta.findUnique({
-        where: { id },
+            where: { id },
+            include: { 
+                vi_producto_fruta: {
+                    include: { vi_producto: true },
+                    where: { estaactivo: true },
+                }
+            },
         });
     }
 
-    //Modo prueba
-    async createFruta(nombre: string, descripcion: string): Promise<vi_fruta> {
-        return await this.prisma.vi_fruta.create({
+    async createFruta(nombre: string, descripcion?: string, productos?: string[]): Promise<vi_fruta> {
+        const frutaId = randomUUID();
+
+        const fruta = await this.prisma.vi_fruta.create({
             data: {
-              id: randomUUID(),
+              id: frutaId,
               nombre: nombre,
-              descripcion: descripcion ,
+              descripcion: descripcion ?? null ,
               urlimagen: "", //validar
               usuariocreacion: "", //validar
             },
-          });
+        });
+
+        if (productos && productos.length > 0) {
+            const productosAgregarData = productos.map(productoId => ({
+                id_producto: productoId,
+                id_fruta: frutaId,
+                estaactivo: true,
+                creadoen: new Date(),
+                actualizadoen: new Date(),
+                usuariocreacion: "", //validar
+            }));
+
+            await this.prisma.vi_producto_fruta.createMany({
+                data: productosAgregarData,
+            });
+        }
+
+        return await this.prisma.vi_fruta.findUnique({
+            where: { id: frutaId },
+            include: { vi_producto_fruta: true },
+        });
+
     }
 
-    /*
-    async updateFruta(id: string, data: Partial<vi_fruta>): Promise<vi_fruta> {
-        return this.prisma.vi_fruta.update({
+    
+    async updateFruta(
+        id: string, 
+        nombre?: string,
+        descripcion?: string,
+        productosParaAgregar?: string[],
+        productosParaQuitar?: string[]
+        ): Promise<vi_fruta> {
+        const data: any = {};
+
+        if (nombre) data.nombre = nombre;
+        if (descripcion) data.descripcion = descripcion;
+
+        const fruta = await this.prisma.vi_fruta.update({
             where: { id },
             data: {
               ...data,
               actualizadoen: new Date(),
             },
-          });
+        });
+
+        //Agregamos productos si hay
+        if (productosParaAgregar && productosParaAgregar.length > 0) {
+            const productosAgregarData = productosParaAgregar.map(productoId => ({
+                id_producto: productoId,
+                id_fruta: id,
+                estaactivo: true,
+                creadoen: new Date(),
+                actualizadoen: new Date(),
+                usuariocreacion: "", // validar/corregir despues
+            }));
+    
+            await this.prisma.vi_producto_fruta.createMany({
+                data: productosAgregarData,
+            });
+        }
+
+        //Quitamos productos si hay
+        if (productosParaQuitar && productosParaQuitar.length > 0) {
+            await this.prisma.vi_producto_fruta.updateMany({
+                where: {
+                    id_fruta: id,
+                    id_producto: { in: productosParaQuitar },
+                },
+                data: {
+                    estaactivo: false,
+                    desactivadoen: new Date(),
+                    actualizadoen: new Date(),
+                    usuarioactualizacion: "", // validar/corregir despues
+                },
+            });
+        }
+        return this.prisma.vi_fruta.findUnique({
+            where: { id },
+            include: {
+                vi_producto_fruta: {
+                    include: {
+                        vi_producto: true, 
+                    },
+                    where: {
+                        estaactivo: true,
+                    }
+                },
+            },
+        });
+        
     }
 
-    async inactivateFruta(id: string): Promise<vi_fruta> {
+    /*async inactivateFruta(id: string): Promise<fruta > {
         return this.prisma.vi_fruta.update({
         where: { id },
         data: {
             estaactivo: false,
             desactivadoen: new Date(),
         },
+        });
+        await this.prisma.vi_producto_fruta.updateMany({
+            where: { id_fruta: id },
+            data: {
+                estaactivo: false,
+                desactivadoen: new Date(),
+                actualizadoen: new Date(),
+            },
         });
     }*/
 }
