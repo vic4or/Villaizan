fc438d55a32e9ae0702765cdb2d920c23c726499
sunjis <sun.ji@pucp.edu.pe>
2024-11-11 13:50:08 -0500

    Correcciones y estandarización de páginas

--- frontend/villaizan2d/src/app/page.js
+++ frontend/villaizan2d/src/app/page.js
@@ -61,13 +61,6 @@ export default function Home() {
               <h3>Gestión de Canjes</h3>
               <p>Administra los códigos generados por los clientes para canjear recompensas</p>
             </div>
-            <div className="card" onClick={() => window.location.href='/pages/notificaciones/lista'}>  
-              <div className="icon-container">
-                <Icon name="bell" />
-              </div>
-              <h3>Gestión de Notificaciones</h3>
-              <p>Gestiona el envío de notificaciones sobre promociones y la programación de las notificaciones</p>
-            </div>
             <div className="card" onClick={() => window.location.href='/pages/multimedia/lista'}>  
               <div className="icon-container">
                 <Icon name="image" />
