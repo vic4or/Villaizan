a3c2f71cfa5b470eccfd5fe36070936808a5e0ff
JuanGalindo <86995708+tokyoGang123@users.noreply.github.com>
2024-11-28 13:41:49 -0500

    prueba de cookie con console log

--- frontend/canje/src/app/page.tsx
+++ frontend/canje/src/app/page.tsx
@@ -1,20 +1,35 @@
-//import CatalogoProductosSuma from '@/app/pages/catalogoProductosSuma';
-//import Carrito from '@/components/Carrito/Carrito';
-//import HistorialPuntos from '@/components/HistorialPuntos/HistorialPuntos';
-
-"use client";
-
 import React from "react";
+import { useSession } from "next-auth/react"; // Importa useSession
 import CatalogoProductosSuma from "@/app/catalogo/page"; // Asegúrate de que la ruta sea correcta
+import {useEffect} from "react";
 
 const Page: React.FC = () => {
+  const { data: session } = useSession(); // Obtén la sesión de autenticación
+  useEffect(() => {
+    if (session) {
+      console.log(session);
+    }
+  })
   return (
     <div className="min-h-screen bg-gray-50">
-      {/* Renderiza el catálogo aquí */}
-      <CatalogoProductosSuma />
+      {/* Verifica si el usuario está autenticado */}
+      {session ? (
+        <>
+          <p className="text-center text-green-700 font-bold">
+            Bienvenido, {session.user?.name}
+          </p>
+          {/* Renderiza el catálogo solo si el usuario está autenticado */}
+          <CatalogoProductosSuma />
+        </>
+      ) : (
+        <p className="text-center text-red-500 font-bold">
+          Por favor, inicia sesión para acceder al catálogo.
+        </p>
+      )}
     </div>
   );
 };
 
 export default Page;
+
 
