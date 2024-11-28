a6d4b64935933ceecaa1c3080da13437573128f2
JuanGalindo <86995708+tokyoGang123@users.noreply.github.com>
2024-11-28 13:05:40 -0500

    .

--- frontend/canje/src/app/api/mainAxios.api.tsx
+++ frontend/canje/src/app/api/mainAxios.api.tsx
@@ -5,11 +5,11 @@ const baseApi = axios.create({
     //Descomentar URL a usar y comentar URL que ya no vas a usar
 
     //URL PARA PRUEBAS LOCALES
-    baseURL: `http://localhost:3000/`
+    //baseURL: `http://localhost:3000/`
     //URL PARA EL HOSTEADO EN EL V
     //baseURL: `http://inf226-982-6f.inf.pucp.edu.pe/api/`
     //URL HTTPS
-    //baseURL: `https://apicanje.heladosvillaizan.tech/`
+    baseURL: `https://apicanje.heladosvillaizan.tech/`
 })
 
 export default baseApi;
